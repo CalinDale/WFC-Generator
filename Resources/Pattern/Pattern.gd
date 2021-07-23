394f801ba09ac5d1658652e83661294818c53e2d
CalinDale <citdale@gmail.com>
2021-07-23 17:21:25 +1200

    tweak

--- Resources/Pattern/Pattern.gd
+++ Resources/Pattern/Pattern.gd
@@ -17,15 +17,13 @@ var _center : Vector2
 var _rotated_center : Vector2
 
 
-##Currently having issues here due to using setters and getters in the init that rely on data which hasn't been set yet.
-
-func _init(p_data := {}, p_size := Vector2.ZERO, p_rotatable := true, p_mirrorable_h := true, p_mirrorable_v := true,  p_commonality := 1) -> void:
-	commonality = p_commonality
+func _init(p_data := {}, p_size := Vector2.ZERO, p_rotatable := true, p_mirrorable_h := true, p_mirrorable_v := true,  base_commonality := 1) -> void:
+	commonality = base_commonality
 	rotatable = p_rotatable
 	mirrorable_h = p_mirrorable_h
 	mirrorable_v = p_mirrorable_v
 	self._size = p_size
-	_data = p_data
+	self._data = p_data
 
 
 func _to_string() -> String:
@@ -34,21 +32,25 @@ func _to_string() -> String:
 
 func set_data(p_data: Dictionary) -> void:
 	_data = p_data
-	_string = _stringify(p_data, _size)
-	_variation_strings = _generate_variation_strings(p_data)
+	if not _data.empty:
+		_string = _stringify(p_data, _size)
+		_variation_strings = _generate_variation_strings(p_data)
+	else:
+		_string = "empty pattern"
+		_variation_strings = []
 
 
 func set_size(p_size: Vector2) -> void:
 	_size = p_size
 	_rotated_size = Vector2(p_size.y, p_size.x)
-	_center = Vector2((p_size.x - 1) / 2, (p_size.y - 1) / 2)
-	_rotated_center = Vector2((_rotated_size.x - 1) / 2, (_rotated_size.y - 1) / 2)
+	_center = Vector2(min(0, (p_size.x - 1) / 2), min(0, (p_size.y - 1) / 2))
+	_rotated_center = Vector2(min(0, (_rotated_size.x - 1) / 2), min(0, (_rotated_size.y - 1) / 2))
 
 
 func _stringify(p_data: Dictionary, p_size: Vector2) -> String:
 	var stringified_data := ""
 	if p_size.x == 0 or p_size.y == 0:
-		stringified_data = "empty pattern"
+		stringified_data = "0 size pattern"
 	else:
 		for x in p_size.x:
 			for y in p_size.y:
@@ -117,7 +119,6 @@ func _mirror_h(p_data: Dictionary, p_size: Vector2, p_center: Vector2) -> Dictio
 			mirrored_data[destination_coords] = p_data[origin_coords]
 	return mirrored_data
 
-
 func _mirror_h_point(point: Vector2, p_center: Vector2) -> Vector2:
 	var relative_to_center := point - p_center
 	var mirrored_point = Vector2(-relative_to_center.x, relative_to_center.y) + p_center
@@ -132,7 +133,6 @@ func _mirror_v(p_data: Dictionary, p_size: Vector2, p_center: Vector2) -> Dictio
 			var origin_coords := _mirror_v_point(destination_coords, p_center)
 			mirrored_data[destination_coords] = p_data[origin_coords]
 	return mirrored_data
-
 
 func _mirror_v_point(point: Vector2, p_center: Vector2) -> Vector2:
 	var relative_to_center := point - p_center
